d5fccc746b1d77f02900b9b78afe4e3f704f83a9
TJ Stifter <tjstifter@gmail.com>
2023-05-25 10:59:47 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textItem = document.getElementById('text');
+    //console.log(textItem.textContent);
+    textItem.textContent = 'This is really cool!';
+   // console.log(textItem);
+})
